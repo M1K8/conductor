f795810635fc10136a651b5000f8b655464e527c
M1K <michael@m1k.me>
2024-09-25 19:30:29 +0100

    some clients and actual err handling...

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,7 @@ use tokio;
 
 use std::{
     io::{self, stdout},
+    net::Ipv4Addr,
     time::Duration,
 };
 
@@ -17,8 +18,23 @@ use crossterm::event::{DisableMouseCapture, EnableMouseCapture};
 
 use tokio::sync::mpsc;
 
+pub mod bambu;
 pub mod input;
+pub mod moonraker;
 pub mod ui;
+
+fn cleanup(e: Option<String>) -> io::Result<()> {
+    disable_raw_mode()?;
+    stdout().execute(DisableMouseCapture)?;
+    stdout().execute(LeaveAlternateScreen)?;
+    match e {
+        Some(s) => {
+            println!("💥‼️💥 Conductor exited with error {:?}", s)
+        }
+        None => {}
+    }
+    Ok(())
+}
 
 #[tokio::main]
 async fn main() -> io::Result<()> {
@@ -26,6 +42,17 @@ async fn main() -> io::Result<()> {
     stdout().execute(EnterAlternateScreen)?;
     stdout().execute(EnableMouseCapture)?;
     let mut terminal = Terminal::new(CrosstermBackend::new(stdout()))?;
+    let moon = moonraker::Moonraker::new();
+    let res = moon
+        .test_ping(std::net::IpAddr::V4(Ipv4Addr::new(10, 0, 2, 11)))
+        .await;
+
+    match res {
+        Ok(_) => {}
+        Err(e) => {
+            return cleanup(Some(e.to_string()));
+        }
+    }
 
     let (tx, mut rx) = mpsc::channel(1);
 
@@ -49,8 +76,5 @@ async fn main() -> io::Result<()> {
         }
     }
 
-    disable_raw_mode()?;
-    stdout().execute(DisableMouseCapture)?;
-    stdout().execute(LeaveAlternateScreen)?;
-    Ok(())
+    cleanup(None)
 }
